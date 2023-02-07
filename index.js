03e4ff87dab1ddeb15b09394722b63913b7c7267
Ashish <102414529+Aashishweb@users.noreply.github.com>
2023-02-07 20:44:45 +0530

    Completed Project Today

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 import { tweetsData } from "./data.js";
-const tweetInput = document.getElementById("tweet-input");
-const tweetBtn = document.getElementById("tweet-btn");
-
-tweetBtn.addEventListener("click", function () {
-  console.log(tweetInput.value);
-});
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
 
 document.addEventListener("click", function (e) {
   if (e.target.dataset.like) {
     handleLikeClick(e.target.dataset.like);
   } else if (e.target.dataset.retweet) {
     handleRetweetClick(e.target.dataset.retweet);
+  } else if (e.target.dataset.reply) {
+    handleReplyClick(e.target.dataset.reply);
+  } else if (e.target.id === "tweet-btn") {
+    handleTweetBtnClick();
+  } else if (e.target.dataset.close) {
+    handleCloseClick(e.target.dataset.close);
   }
 });
 
@@ -43,6 +44,35 @@ function handleRetweetClick(tweetId) {
   render();
 }
 
+function handleReplyClick(replyId) {
+  document.getElementById(`replies-${replyId}`).classList.toggle("hidden");
+}
+
+function handleCloseClick() {
+  tweetsData.shift();
+  render();
+}
+
+function handleTweetBtnClick() {
+  const tweetInput = document.getElementById("tweet-input");
+
+  if (tweetInput.value) {
+    tweetsData.unshift({
+      handle: `@Scrimba`,
+      profilePic: `images/scrimbalogo.png`,
+      likes: 0,
+      retweets: 0,
+      tweetText: tweetInput.value,
+      replies: [],
+      isLiked: false,
+      isRetweeted: false,
+      uuid: uuidv4(),
+    });
+    render();
+    tweetInput.value = "";
+  }
+}
+
 function getFeedHtml() {
   let feedHtml = ``;
 
@@ -58,10 +88,27 @@ function getFeedHtml() {
     if (tweet.isRetweeted) {
       retweetIconClass = "retweeted";
     }
+    let repliesHtml = "";
+    if (tweet.replies.length > 0) {
+      tweet.replies.forEach(function (reply) {
+        repliesHtml += `
+<div class="tweet-reply">
+<div class="tweet-inner">
+  <img src="${reply.profilePic}" class="profile-pic">
+      <div>
+          <p class="handle">${reply.handle}</p>
+          <p class="tweet-text">${reply.tweetText}</p>
+      </div>
+  </div>
+</div>
+`;
+      });
+    }
 
     feedHtml += `
 <div class="tweet">
     <div class="tweet-inner">
+    <span data-close="${tweet.uuid}" class="tweet-close">&times;</span>
         <img src="${tweet.profilePic}" class="profile-pic">
         <div>
             <p class="handle">${tweet.handle}</p>
@@ -82,7 +129,11 @@ function getFeedHtml() {
             </div>   
         </div>            
     </div>
+    <div class="hidden" id="replies-${tweet.uuid}">
+        ${repliesHtml}
+    </div>  
 </div>
+
 `;
   });
   return feedHtml;
